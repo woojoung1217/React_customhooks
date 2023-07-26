18adddceda14e1e7da70b683859bdfa11a9315bb
woojoung <ywj981217@naver.com>
2023-07-27 02:17:23 +0900

    edit hooks

--- src/Customhooks/useScroll.js
+++ src/Customhooks/useScroll.js
@@ -26,7 +26,7 @@ const App = () => {
   const { y } = useScroll();
   return (
     <div style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "green" : "blue" }}>
         hello
       </h1>
     </div>
